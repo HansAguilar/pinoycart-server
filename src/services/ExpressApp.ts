780d0df402010948077caf8b6bd5e9b32101fc18
Jas Aguilar <aguilarhans100701@gmail.com>
2024-04-20 17:01:02 +0800

    cors

--- src/services/ExpressApp.ts
+++ src/services/ExpressApp.ts
@@ -1,4 +1,4 @@
-import express, { Application, Response } from "express";
+import express, { Application, Response, NextFunction } from "express";
 import cors from "cors";
 import { ItemRoutes, UserRoutes, VendorRoutes, PaymentRoutes } from "../routes";
 import cookieParser from 'cookie-parser';
@@ -6,20 +6,23 @@ import cookieParser from 'cookie-parser';
 export default async (app: Application) => {
     app.use(cors({
         credentials: true,
-        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
         origin: 'https://pinoycart-client.vercel.app',
-    })); //! this solves the cors problem in frontend
+    }));
     app.use(cookieParser());
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
-    app.use((req, res: Response, next) => {
-        res.setHeader("Access-Control-Allow-Origin", "*")
-        next();
-    });
 
     app.use('/uploads', express.static('uploads'));
 
     app.use("/api/v1", [UserRoutes, VendorRoutes, ItemRoutes, PaymentRoutes]);
+
+    // Set CORS headers for all routes
+    app.use((req, res: Response, next: NextFunction) => {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+        next();
+    });
 
     return app;
 };
