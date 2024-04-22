bbd0dffd2e6f122ff51a2405587be2cd1c7b308b
HansAguilar <aguilarhans100701@gmail.com>
2024-04-22 14:17:14 +0800

    vercel json

--- src/services/ExpressApp.ts
+++ src/services/ExpressApp.ts
@@ -9,7 +9,11 @@ export default async (app: Application) => {
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
 
-    app.use('/uploads', express.static('uploads'));
+    // app.use('/uploads', express.static('uploads'));
+
+    app.get("/", (req, res) => {
+        res.json({message: "fuck u"})
+    })
 
     app.use("/api/v1", [UserRoutes, VendorRoutes, ItemRoutes, PaymentRoutes]);
 
