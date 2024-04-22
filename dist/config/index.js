92a6033f3b6be552ba002e1422b95bc454194ff8
HansAguilar <aguilarhans100701@gmail.com>
2024-04-22 13:04:57 +0800

    vercel json

--- dist/config/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.APP_X_KEY = exports.MONGO_URI = void 0;
-exports.MONGO_URI = "mongodb://127.0.0.1:27017/PinoyCartDB";
-exports.APP_X_KEY = "PRXFNATICSENTINELSTSEGEDGEGDRX";
-//# sourceMappingURL=index.js.map
--- dist/src/utility/CalculationsUtil.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.applyRandomDiscount = void 0;
-const applyRandomDiscount = (originalPrice) => {
-    return originalPrice - 1;
-};
-exports.applyRandomDiscount = applyRandomDiscount;
-//# sourceMappingURL=CalculationsUtil.js.map
