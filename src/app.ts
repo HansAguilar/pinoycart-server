7c6ae594374a2296b9a19912337ae2acba37324d
HansAguilar <aguilarhans100701@gmail.com>
2024-04-22 22:25:49 +0800

    app listener

--- src/app.ts
+++ src/app.ts
@@ -3,13 +3,14 @@ import ExpressApp from "./services/ExpressApp";
 import Database from "./services/Database";
 
 const app = express();
+const port = 3000;
 //
 const StartServer = async () => {
 
     await Database();
     await ExpressApp(app);
 
-    app.listen(3000, () => {
+    app.listen(port, "0.0.0.0", () => {
         console.log("Server running");
     })
 };
